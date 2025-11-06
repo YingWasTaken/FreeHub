bde0186df76883b59a8639e93931ccc0789f41c5
YingWasTaken <casamianquinteropablo@gmail.com>
2025-11-06 22:12:27 +0100

    burger menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,6 @@
 import "./Navbar.scss";
-
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
 import categoriesData from "../../assets/db/categories.json";
 import NavButton from "../NavButton/NavButton";
 
@@ -15,23 +13,41 @@ type Category = {
 
 export default function Navbar() {
     const [categories, setCategories] = useState<Category[]>([]);
+    const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
         setCategories(categoriesData);
     }, []);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     return (
         <nav className="navbar">
             <Link to="/" className="navbar-logo">
                 FreeHub
             </Link>
-            <div className="navbar-links">
+
+            {/* Botón de hamburguesa */}
+            <div
+                className={`navbar-hamburger ${menuOpen ? "open" : ""}`}
+                onClick={toggleMenu}
+            >
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+
+            {/* Links de navegación */}
+            <div className={`navbar-links ${menuOpen ? "active" : ""}`}>
                 {categories.map((cat) => (
                     <NavButton
                         key={cat.id}
                         name={cat.name}
                         icon={cat.icon}
                         path={`/${cat.name.toLowerCase().replace(/\s+/g, "-")}`}
+                        onClick={() => setMenuOpen(false)} // cerrar menú al hacer click
                     />
                 ))}
             </div>
